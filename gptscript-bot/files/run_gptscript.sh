2bc60d1005457148e450bc1716fba808aafe2d1a
VictorGetz <victor.getz@iits-consulting.de>
2024-09-05 12:14:23 +0200

    containerdays demo

--- gptscript-bot/files/run_gptscript.sh
+++ gptscript-bot/files/run_gptscript.sh
@@ -1,11 +1,5 @@
 #bin/bash
 
-configure_git() {
-    git remote set-url origin "$GIT_URL"
-    git config --global user.email "$BOT_EMAIL"
-    git config --global user.name "$BOT_NAME"
-    git checkout "$COMMIT_REF"
-}
 
 process_commands() {
     echo "$COMMANDS_JSON" | jq -c '.[]' | while read -r cmd; do
@@ -21,8 +15,6 @@ process_commands() {
         gptscript --default-model "$model" --workspace "$(dirname "$0")" --disable-cache --openai-base-url "$OPENAI_BASE_URL" --openai-api-key "$OPENAI_API_KEY" "$botConfig" "$command_string"
     done
 }
-
-configure_git
 
 #Workdir
 mkdir -p tmp/gptscript
